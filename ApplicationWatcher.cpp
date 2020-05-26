e0f9d9d6cfab2ac6873f2e3504b1ea2f444ec5ba
Wei Lisi <tlsirius@163.com>
2020-05-26 21:08:05 +0800

    optimize screen capturing

--- ApplicationWatcher.cpp
+++ ApplicationWatcher.cpp
@@ -4,7 +4,7 @@
 RECT ApplicationWatcher::getWindowSize()
 {
     RECT window_rect = { 0 };
-    GetClientRect(m_appHandle, &window_rect);
+    GetWindowRect(m_appHandle, &window_rect);
     return window_rect;
 }
 
@@ -18,21 +18,44 @@ void ApplicationWatcher::setApplication(QString windowTitle)
     delete[] wcharTitle;
 }
 
-PIX* ApplicationWatcher::capture(RECT roi) {
+PIX* ApplicationWatcher::capture(RECT roi) 
+{
     RECT client_rect = { 0 };
     GetClientRect(m_appHandle, &client_rect);
     int windowWidth = client_rect.right - client_rect.left;
     int windowHeight = client_rect.bottom - client_rect.top;
+//
+//    HDC hdcScreen = GetDC(m_appHandle);
+//    HDC hdc = CreateCompatibleDC(hdcScreen);
+//    HBITMAP hbmp = CreateCompatibleBitmap(hdcScreen, windowWidth, windowHeight);
+//    SelectObject(hdc, hbmp);
+//
+//    int width = windowWidth;
+//    int height = windowHeight;
+//
+//    BitBlt(hdc, 0, 0, width, height, hdcScreen, 0, 0, SRCCOPY);
+//
 
-    HDC hdcScreen = GetDC(m_appHandle);
-    HDC hdc = CreateCompatibleDC(hdcScreen);
-    HBITMAP hbmp = CreateCompatibleBitmap(hdcScreen, windowWidth, windowHeight);
-    SelectObject(hdc, hbmp);
+//
+//    BYTE* bmp_pixels = new BYTE[(width * 3 + bmp_padding) * height];
+//    GetDIBits(hdc, hbmp, 0, height, bmp_pixels, &bmp_info, DIB_RGB_COLORS);
 
-    int width = windowWidth;
-    int height = windowHeight;
+    // get the device context of the screen
+    HDC hScreenDC = CreateDC(L"DISPLAY", NULL, NULL, NULL);
+    // and a device context to put it in
+    HDC hMemoryDC = CreateCompatibleDC(hScreenDC);
 
-    BitBlt(hdc, 0, 0, width, height, hdcScreen, 0, 0, SRCCOPY);
+    int width = GetDeviceCaps(hScreenDC, HORZRES);
+    int height = GetDeviceCaps(hScreenDC, VERTRES);
+
+    // maybe worth checking these are positive values
+    HBITMAP hBitmap = CreateCompatibleBitmap(hScreenDC, width, height);
+
+    // get a new bitmap
+    HBITMAP hOldBitmap = (HBITMAP)SelectObject(hMemoryDC, hBitmap);
+
+    BitBlt(hMemoryDC, 0, 0, width, height, hScreenDC, 0, 0, SRCCOPY);
+    hBitmap = (HBITMAP)SelectObject(hMemoryDC, hOldBitmap);
 
     BITMAPINFO bmp_info = { 0 };
     bmp_info.bmiHeader.biSize = sizeof(bmp_info.bmiHeader);
@@ -46,7 +69,11 @@ PIX* ApplicationWatcher::capture(RECT roi) {
     if (bmp_padding != 0) bmp_padding = 4 - bmp_padding;
 
     BYTE* bmp_pixels = new BYTE[(width * 3 + bmp_padding) * height];
-    GetDIBits(hdc, hbmp, 0, height, bmp_pixels, &bmp_info, DIB_RGB_COLORS);
+    GetDIBits(hMemoryDC, hBitmap, 0, height, bmp_pixels, &bmp_info, DIB_RGB_COLORS);
+    
+    // clean up
+    DeleteDC(hMemoryDC);
+    DeleteDC(hScreenDC);
 
     Pix* pixd = pixCreate(width, height, 32);
     for (int y = 0; y < height; y++) {
@@ -54,7 +81,7 @@ PIX* ApplicationWatcher::capture(RECT roi) {
             pixSetRGBPixel(pixd, x, height - y - 1, bmp_pixels[(width * 3 + bmp_padding) * y + 3 * x+2], bmp_pixels[(width * 3 + bmp_padding) * y + 3 * x+1], bmp_pixels[(width * 3 + bmp_padding) * y + 3 * x]);
         }
     }
-    //pixWrite("D:/pixd.png", pixd, IFF_PNG);
+    pixWrite("D:/pixd.png", pixd, IFF_PNG);
 
     delete[] bmp_pixels;
     return pixd;
