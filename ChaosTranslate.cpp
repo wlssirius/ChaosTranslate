d99384d2cf060cc681d72daa387d59de7e157384
Wei Lisi <tlsirius@163.com>
2020-08-10 22:51:11 +0800

    fix regional capture

--- ChaosTranslate.cpp
+++ ChaosTranslate.cpp
@@ -51,7 +51,9 @@ void ChaosTranslate::captureAndTranslate(bool clicked)
 	std::shared_ptr<PIX> pix = captureApp();
 
 	emit setOriginalText("Recognizing");
+	pixWrite("test1.png", pix.get(), IFF_PNG);
 	processImg(pix);
+	pixWrite("test.png", pix.get(), IFF_PNG);
 	ocrTranslate(pix);
 };
 
@@ -218,7 +220,7 @@ void ChaosTranslate::processImg(std::shared_ptr<PIX> pix)
 	}
 	m_capturedImage = convertPixToQImage(pix);
 	m_imageLabel->setPixmap(QPixmap::fromImage(*m_capturedImage));
-	if (m_regionalCapture)
+	if (m_manualSetFontColor)
 	{
 		thresholdByFontColor(pix.get());
 	}
